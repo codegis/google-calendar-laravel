580baa7e11d0309976d98b27de8a77fec58c0400
Luis Diaz <ldiazjaramillo@gmail.com>
2017-07-08 11:27:47 -0400

    updating to google api 2.0

--- src/GoogleCalendarFactory.php
+++ src/GoogleCalendarFactory.php
@@ -11,14 +11,26 @@ class GoogleCalendarFactory
     {
         $config = config('laravel-google-calendar');
 
+        $scopes = implode(' ', array( Google_Service_Calendar::CALENDAR ) );
+        $credentialsPath = expandHomeDirectory($config['client_secret_json']);
+        $application_name = "Job target calendar";
+
         $client = new Google_Client();
 
-        $credentials = $client->loadServiceAccountJson(
-            $config['client_secret_json'],
-            'https://www.googleapis.com/auth/calendar'
-        );
+        $client->setApplicationName($application_name);
+        $client->setScopes($scopes);
+        $client->setAuthConfig($config['client_secret_json']);
+        $client->setAccessType('offline');
 
-        $client->setAssertionCredentials($credentials);
+        $accessToken = json_decode(file_get_contents($credentialsPath), true);
+        $client->setAccessToken($accessToken);
+
+        // Refresh the token if it's expired.
+        if ($client->isAccessTokenExpired()) {
+            $client->fetchAccessTokenWithRefreshToken($client->getRefreshToken());
+            file_put_contents($credentialsPath, json_encode($client->getAccessToken()));
+        }
+        //$client->setAssertionCredentials($credentials);
 
         $service = new Google_Service_Calendar($client);
 
